41b4b1a7b32d7c452195128e862bff1eb739fdd9
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2026-04-24 11:12:53 +0100

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,8 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    private float timer = 3f;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
